3ab4019577ba69d8170240b54bfd60f0d4a33178
NipunChamika <nipunchamika11@gmail.com>
2024-05-07 12:55:18 +0530

    feat: Enable CORS

--- store/validators.py
+++ store/validators.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 
 
 def validate_file_size(file):
-    file_size_kb = 1000
+    file_size_kb = 2500
 
     if file.size > file_size_kb * 1024:
         raise ValidationError(f'Files cannot be larger than {file_size_kb}KB!')
