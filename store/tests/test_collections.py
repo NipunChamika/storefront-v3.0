f6bbdd8fb50cc0720f40920528c6bb4f1be53f19
Nipun Chamika <nipunchamika11@gmail.com>
2024-05-09 16:48:16 +0530

    feat: Use fixtures to remove duplication in test code

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,51 +1,52 @@
-from django.contrib.auth.models import User
 from rest_framework import status
-from rest_framework.test import APIClient
 import pytest
+
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+    return do_create_collection
 
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    def test_if_user_is_anonymous_returns_401(self):
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
         # Arrange
 
         # Act
-        client = APIClient()
-        response = client.post('/store/collections/', {'title': 'a'})
+        response = create_collection({'title': 'a'})
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_returns_403(self):
+    def test_if_user_is_not_admin_returns_403(self, create_collection, authenticate):
         # Arrange
+        authenticate()
 
         # Act
-        client = APIClient()
-        client.force_authenticate(user={})
-        response = client.post('/store/collections/', {'title': 'a'})
+        response = create_collection({'title': 'a'})
 
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_returns_400(self):
+    def test_if_data_is_invalid_returns_400(self, create_collection, authenticate):
         # Arrange
+        authenticate(is_staff=True)
 
         # Act
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': ''})
+        response = create_collection({'title': ''})
 
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_if_data_is_valid_returns_201(self):
+    def test_if_data_is_valid_returns_201(self, create_collection, authenticate):
         # Arrange
+        authenticate(is_staff=True)
 
         # Act
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': 'a'})
+        response = create_collection({'title': 'a'})
 
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
